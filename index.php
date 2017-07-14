3802b717334c49a2cdb02005d88bfa66c6b49081
Seyi Onifade <xyluz@ymail.com>
2017-07-14 11:56:31 +0100

    return

--- index.php
+++ index.php
@@ -25,7 +25,16 @@ if(isset($_POST['total_length'])){
 
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-		
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-8808111-6', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
 	</head>
 	
 	
