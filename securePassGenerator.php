e5e939cf98fc935d9d486e8fbda77a8a26e18414
Seyi Onifade <xyluz@ymail.com>
2017-07-14 12:02:44 +0100

    added more symbols

--- securePassGenerator.php
+++ securePassGenerator.php
@@ -14,7 +14,7 @@ function generate($total_Length, $uppercase_length, $lowercase_length){
 	$UPPER_CASE = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 	$lower_case = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 	$numbers = ["1","2","3","4","5","6","7","8","9","0"];
-	$symbols = ["*","#","@","!","(",")","&","^","%","$"];
+	$symbols = ["*","#","@","!","(",")","&","^","%","$","~","/","]","[","{","}","?",">","<",";"];
 
 	$u = randomizeArray($UPPER_CASE,$uppercase_length);
 	$l = randomizeArray($lower_case,$lowercase_length);
